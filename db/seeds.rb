0e948a558b6b7f6ee0bd5ac73e407b7d48931818
donna <donnacamos88@gmail.com>
2019-10-31 19:30:22 -0400

    initial setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Photo.create(image_url: "https://unsplash.com/photos/XtRVivoKXfs")
