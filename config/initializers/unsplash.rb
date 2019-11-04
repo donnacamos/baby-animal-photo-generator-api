4f6fa32b9be22ddf5d6676b157fde3122d95f025
donna <donnacamos88@gmail.com>
2019-11-04 11:31:47 -0500

    updated unsplash.rb

--- config/initializers/unsplash.rb
+++ config/initializers/unsplash.rb
@@ -2,8 +2,9 @@ Unsplash.configure do |config|
     config.application_access_key = "YOUR ACCESS KEY"
     config.application_secret = "YOUR APPLICATION SECRET"
     config.application_redirect_uri = "https://your-application.com/oauth/callback"
-    config.utm_source = "alices_terrific_client_app"
+    config.utm_source = "baby-animal-photo-generator-app" 
   
     # optional:
     config.logger = MyCustomLogger.new
   end
+
